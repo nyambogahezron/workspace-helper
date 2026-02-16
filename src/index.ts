bffbb3a73ef718c110969e8f407164d2a9e8d163
Hezron Nyamboga <hezzyn1@gmail.com>
2026-02-16 14:20:12 +0300

    style: Enforce consistent formatting by changing string literals to double quotes and adding semicolons.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-import { isCancel, outro, select } from '@clack/prompts'
-import AlignVersions from './align-version'
-import CreateRelease from './create-release'
-import printLogo from './utils/logo'
+import { isCancel, outro, select } from "@clack/prompts";
+import AlignVersions from "./align-version";
+import CreateRelease from "./create-release";
+import printLogo from "./utils/logo";
 
 async function main() {
-	printLogo()
+	printLogo();
 
 	const command = await select({
-		message: 'Choose a command:',
+		message: "Choose a command:",
 		options: [
-			{ value: 'manage-release', label: 'Manage Releases' },
-			{ value: 'align-versions', label: 'Align Versions' },
+			{ value: "manage-release", label: "Manage Releases" },
+			{ value: "align-versions", label: "Align Versions" },
 		],
-	})
+	});
 
 	if (isCancel(command)) {
-		outro('Operation cancelled')
-		process.exit(0)
+		outro("Operation cancelled");
+		process.exit(0);
 	}
 
-	if (command === 'manage-release') {
-		await CreateRelease()
-	} else if (command === 'align-versions') {
-		const alignVersions = new AlignVersions()
-		await alignVersions.init()
-		await alignVersions.run()
+	if (command === "manage-release") {
+		await CreateRelease();
+	} else if (command === "align-versions") {
+		const alignVersions = new AlignVersions();
+		await alignVersions.init();
+		await alignVersions.run();
 	}
 }
 
-main().catch(console.error)
+main().catch(console.error);
